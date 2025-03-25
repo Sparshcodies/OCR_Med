ee8d03799f72c00c1a6b6ffd57059d2409adff38
Sparsh <sparshsahu567@gmail.com>
2025-03-25 14:17:34 +0530

    added login/logout functionalities

--- OSM_maps/urls.py
+++ OSM_maps/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import map_view, get_pharmacies
+from .views import map_view, get_pharmacies, register_view, login_view, logout_view
 
 urlpatterns = [
-    path('', map_view, name='map'),
+    path("", login_view, name="login"),
+    path("register/", register_view, name="register"),
+    path("logout/", logout_view, name="logout"),
+    path("dashboard/", map_view, name="dashboard"),
     path('api/pharmacies/', get_pharmacies, name='get_pharmacies'),
 ]
--- OSM_maps/views.py
+++ OSM_maps/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import Pharmacy
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 
+
+@login_required
 def map_view(request):
-    return render(request, 'OSM_maps/map.html')
+    return render(request, "user_auth/dashboard.html", {"user": request.user})
 
 def get_pharmacies(request):
     pharmacies = Pharmacy.objects.all()
@@ -18,3 +23,31 @@ def get_pharmacies(request):
         for pharmacy in pharmacies
     ]
     return JsonResponse({"pharmacies": data})
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request, "user_auth/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")  # Redirect to map view after successful login
+        else:
+            return render(request, "user_auth/login.html", {"error": "Invalid credentials"})
+    return render(request, "user_auth/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
